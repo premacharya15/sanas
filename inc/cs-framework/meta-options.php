c72b78652f6305450a51745ef3e1d2c22f1ee3dc
prem-credsoft <premcredsoft@gmail.com>
2024-11-14 18:13:09 +0530

    Update meta-options.php

--- inc/cs-framework/meta-options.php
+++ inc/cs-framework/meta-options.php
@@ -127,12 +127,6 @@ if( class_exists( 'CSF' ) ) {
         'title' => 'Back Template Images in Gallery',
       )
 	  ,
-	//   array(
-	// 	 'id'    => 'card_category_cover_gallery',
-	// 	 'type'  => 'gallery',
-	// 	 'title' => 'Card Category Cover Images',
-	//    )
-    //  ,
      array(
         'id'    => 'card_category_home',
         'type'  => 'switcher',
