fd66015fc08154b4cb5fcef5080f2b2df66f0031
Prem Acharya <premcredsoft@gmail.com>
2024-11-15 15:35:03 +0530

    Update taxonomy-sanas-card-category.php

--- taxonomy-sanas-card-category.php
+++ taxonomy-sanas-card-category.php
@@ -36,7 +36,7 @@ $cover_card_image_url = wp_get_attachment_url($cover_card_image_id);
         </div>
         <div class="col-md-6 xol-sm-12 wl-bwnnar-img">
           <?php if ($cover_card_image_url) : ?>
-            <img src="<?php echo esc_url($cover_card_image_url); ?>">
+            <img src="<?php echo esc_url($sanas_portfolio_meta['cover_card_image']['url']); ?>" alt="banner-image">
           <?php endif; ?>
         </div>
       </div>
