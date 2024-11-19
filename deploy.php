657e0ec690fb0f753126d56852789aa8ca37b69b
Prem Acharya <premcredsoft@gmail.com>
2024-11-19 18:14:26 +0530

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -1,5 +1,5 @@
 <?php
-$dir = '/home4/sanashub/home4/sanasinvite.com/public_html/.dev132/wp-content/themes/sanas';
+$dir = '/home4/sanashub/sit132.sanasinvite.com/wp-content/themes/sanas';
 chdir($dir);
 $output = shell_exec('git fetch --all && git reset --hard origin/main 2>&1');
 echo "<pre>$output</pre>";
