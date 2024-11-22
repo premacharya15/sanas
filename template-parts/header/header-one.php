eb1fa76e362fc2bea89237f611174dc7f77706a4
Prem Acharya <premcredsoft@gmail.com>
2024-11-22 16:17:43 +0530

    Update header-one.php

--- template-parts/header/header-one.php
+++ template-parts/header/header-one.php
@@ -7,7 +7,16 @@ $loginEnable  = sanas_options('sanas_header_login_button_enable');
 $loginText    = sanas_options('sanas_header_login_button_text');
 
 $class_fixed = 'wl-header';
-if ( is_page_template( 'page-template/myevent.php' ) ) {
+if(is_page_template( 'page-template/myevent.php')
+|| is_page_template( 'page-template/wishlist.php')
+|| is_page_template( 'page-template/todolist.php')
+|| is_page_template( 'page-template/my-vendors.php')
+|| is_page_template( 'page-template/vendor-list.php')
+|| is_page_template( 'page-template/mycontact.php')
+|| is_page_template( 'page-template/my-profile.php')
+|| is_page_template( 'page-template/my-dashboard.php')
+|| is_page_template( 'page-template/budget.php')
+) {
   $class_fixed = 'position-fixed';
 }
 
@@ -30,24 +39,34 @@ $first_char = substr($current_user->user_firstname, 0, 1);
             <div class="user">
               <div class="inner-colum justify-content-end dropdown">
                   <div class="btn-profile dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="true">
-                      <?php echo ucfirst($first_char); ?>
+                      <?php
+                      $profile_picture = get_user_meta($userID, 'profile_picture', true);
+                      if (!empty($profile_picture)) {
+                          echo '<img class="user-profile-image" src="' . esc_url($profile_picture) . '" alt="Profile Picture">';
+                      } else {
+                          echo ucfirst($first_char);
+                      }
+                      ?>
                   </div>                
                 <img class="btn btn-secondary dropdown-toggle" style="display:none;" data-bs-toggle="dropdown" aria-expanded="false"
                   src="<?php echo get_template_directory_uri(); ?>/assets/img/login-img.jpg" alt="login-person">
                 <ul class="dropdown-menu p-0">
                   <li>Nancy</li>
-                  <li><a href="#"> Dashboard</a></li>
-                  <li><a href="<?php echo home_url(); ?>/myevent"> My Events</a></li>
-                  <li><a href="<?php echo esc_url($logout_url);?>">Logout</a></li>
+                  <li><a href="<?php echo home_url(); ?>/my-dashboard">Dashboard</a></li>
+                  <li><a href="<?php echo home_url(); ?>/my-events"> My Events</a></li>
+                  <li class="logout-btn"><a href="javascript:void(0);" data-logout-url="<?php echo $logout_url; ?>">Logout</a></li>
                 </ul>
               </div>
             </div>
            <?php } else {                         
+                        
+                  //if ($loginEnable && !empty($loginText)) 
+                  //{
                   ?>
                   <div class="main-menu-btn text-end">
                     <a href="#" class="btn btn-primary login-in sanas-login-popup">Sign In / Sign Up</a>
                   </div>
-            <?php  
+            <?php //} 
               }
              ?>
           </div>
@@ -66,13 +85,16 @@ $first_char = substr($current_user->user_firstname, 0, 1);
                   wp_nav_menu( array(
                   'theme_location' => 'sanas-main-menu',
                   'container' => 'ul',
-                  'menu_class' => 'main-menu-list'));
+                  'menu_class' => 'main-menu-list', ));
                 }
                 else {
                    echo '<div class="no-main-menu"><ul class="not no-menu text-right"><li><a  href="' . esc_url( home_url( '/' ) ) . 'wp-admin/nav-menus.php">' . 
                    esc_html__( 'Set Primary Menu.', 'sanas' ) . '</a></li></ul></div>';
                 }
             ?>
+<?php  /* ?>
+          <li class="menu-item-children"><a href="https://www.sanashub.com/" target="_blank">Desi Vendors</a></li>
+<?php */ ?>
       </nav>
     </div>
   </header>
