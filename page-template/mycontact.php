732722210308d8217c3de567e585fd96e422a77d
Prem Acharya <166224472+premacharya15@users.noreply.github.com>
2024-11-26 15:56:33 +0530

    Update mycontact.php

--- page-template/mycontact.php
+++ page-template/mycontact.php
@@ -32,18 +32,18 @@ $get_event = $wpdb->get_results(
 
 <div class="wl-dashboard-wrapper dashboard">
     <div class="container-fluid wl-dashboard-content">
-      <div class="row">
-        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-          <div class="page-header d-flex justify-content-between align-items-center">
-            <h3 class="pageheader-title mb-0"> My Contacts</h3>
-            <div class="links-box-2 ">
-              <button type="submit" class="dashbord-btn">Move to Guest List </button>
+        <div class="row">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                <div class="page-header d-flex justify-content-between align-items-center">
+                    <h3 class="pageheader-title mb-0">My Contacts</h3>
+                    <div class="links-box-2">
+                        <button type="submit" class="dashbord-btn">Move to Guest List</button>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
 
-      <?php if (!empty($get_event)) : ?>
+        <?php if (!empty($get_event)) : ?>
             <?php 
             $table_counter = 1;
             foreach ($get_event as $event) : ?>
@@ -74,78 +74,58 @@ $get_event = $wpdb->get_results(
                 );
                 ?>
 
-      <div class="guests-list contact">
-        <div class="inner tabs-box guests-tabs">
-          <div class="guests-box tabs-content">
-            <div class="table-responsive">
-              <table class="table data-table display" id="guest-contact-list-<?php echo $table_counter; ?>">
-                <thead>
-                  <tr>
-                    <th class="todo-subhead text-align-start" colspan="6">
-                      <h4><?php echo esc_html($event_name); ?></h4>
-                    </th>
-                  </tr>
-                  <tr>
-                    <th><input type="checkbox" id="all-select-checkbox-<?php echo $table_counter; ?>"> </th>
-                    <th>Name</th>
-                    <th>Phone Number</th>
-                    <th>Email address</th>
-                    <th>Group</th>
-                    <th>Actions</th>
-                  </tr>
-                </thead>
-                <tbody>
-                <?php foreach ($get_guest_details as $guest) : ?>
-                    <tr>
-                        <td><input type="checkbox"></td>
-                        <td><?php echo esc_html($guest->guest_name); ?></td>
-                        <td><?php echo esc_html($guest->guest_phone_num); ?></td>
-                        <td><?php echo esc_html($guest->guest_email); ?></td>
-                        <td><?php echo esc_html($guest->guest_group); ?></td>
-                        <td class="actions">
-                            <div>
-                                <a href="#" class="edit theme-btn" data-bs-toggle="modal" data-bs-target="#edit-popup">
-                                    <i class="fa-solid fa-pen"></i>
-                                </a>
-                                <a href="#" class="delete theme-btn">
-                                    <i class="fa-regular fa-trash-can"></i>
-                                </a>
+                <div class="guests-list contact">
+                    <div class="inner tabs-box guests-tabs">
+                        <div class="guests-box tabs-content">
+                            <div class="table-responsive">
+                                <table class="table data-table display" id="guest-contact-list-<?php echo $table_counter; ?>">
+                                    <thead>
+                                        <tr>
+                                            <th class="todo-subhead text-align-start" colspan="6">
+                                                <h4><?php echo esc_html($event_name); ?></h4>
+                                            </th>
+                                        </tr>
+                                        <tr>
+                                            <th><input type="checkbox" id="all-select-checkbox-<?php echo $table_counter; ?>"></th>
+                                            <th>Name</th>
+                                            <th>Phone Number</th>
+                                            <th>Email address</th>
+                                            <th>Group</th>
+                                            <th>Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php foreach ($get_guest_details as $guest) : ?>
+                                            <tr>
+                                                <td><input type="checkbox"></td>
+                                                <td><?php echo esc_html($guest->guest_name); ?></td>
+                                                <td><?php echo esc_html($guest->guest_phone_num); ?></td>
+                                                <td><?php echo esc_html($guest->guest_email); ?></td>
+                                                <td><?php echo esc_html($guest->guest_group); ?></td>
+                                                <td class="actions">
+                                                    <div>
+                                                        <a href="#" class="edit theme-btn" data-bs-toggle="modal" data-bs-target="#edit-popup">
+                                                            <i class="fa-solid fa-pen"></i>
+                                                        </a>
+                                                        <a href="#" class="delete theme-btn">
+                                                            <i class="fa-regular fa-trash-can"></i>
+                                                        </a>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        <?php endforeach; ?>
+                                    </tbody>
+                                </table>
                             </div>
-                        </td>
-                    </tr>
-                  <?php endforeach; ?>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-        <div class="row align-items-center">
-          <div class="col-xxl-2 col-lg-4  col-sm-6 m-auto">
-          </div>
-        </div>
-      </div>
-      <script>
-        jQuery(document).ready(function($) {
-            $('#guest-contact-list-<?php echo $table_counter; ?>').DataTable({
-                searching: true,
-                paging: true,
-                "order": [],
-                "ordering": true,
-                columnDefs: [
-                    { orderable: false, targets: [0, 2, 3, 4, 5] },
-                ],
-                language: {
-                    info: ""
-                }
-            });
-        });
-      </script>
-      <?php 
-      $table_counter++;
-      endforeach; ?>
-      <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+            <?php 
+            $table_counter++;
+            endforeach; ?>
+        <?php endif; ?>
     </div>
-  </div>
+</div>
 
   <div class="modal fade def-popup add-guest-popup" id="edit-popup" tabindex="-1" role="dialog"
   aria-hidden="true">
