9d4b705583afab812cd81c5be2206cb9eeed57a0
Prem Acharya <166224472+premacharya15@users.noreply.github.com>
2024-11-26 20:08:03 +0530

    Update mycontact.php

--- page-template/mycontact.php
+++ page-template/mycontact.php
@@ -145,10 +145,6 @@ $get_guest_group = $wpdb->get_results(
             <?php 
             $table_counter++;
             endforeach; ?>
-        <?php else : ?>
-            <div class="no-events-message">
-                <p>No events available.</p>
-            </div>
         <?php endif; ?>
     </div>
 </div>
