be729b77d59326abf6a9031ab98065627f758c8c
Prem Acharya <166224472+premacharya15@users.noreply.github.com>
2024-11-26 19:57:46 +0530

    Update mycontact.php

--- page-template/mycontact.php
+++ page-template/mycontact.php
@@ -117,6 +117,7 @@ $get_guest_group = $wpdb->get_results(
                                                         </a>
                                                         <a href="javascript:void(0)" data-guest-id="<?php echo esc_attr($guest->guest_id);?>" class="delete theme-btn delete-guest-details">
                                                             <i class="fa-regular fa-trash-can"></i>
+                                                            <input type="hidden" id="security" name="security" value="<?php echo wp_create_nonce('ajax-delete-guest-nonce'); ?>">
                                                         </a>
                                                     </div>
                                                 </td>
