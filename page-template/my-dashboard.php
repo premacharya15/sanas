61f53c1c0bc69e1088d3fc992b64a722a723e1b9
parthwps <parth@netdevs.net>
2024-12-04 02:06:57 +0530

    Update my-dashboard.php

--- page-template/my-dashboard.php
+++ page-template/my-dashboard.php
@@ -207,11 +207,11 @@ if($total_adults == 0 && $total_kids == 0){
           </div>
         </div>
         <div class="attend-info col-xl-4 col-lg-6 col-md-6 col-sm-6 col-12">
-          <a href="/user-dashboard/?dashboard=guestlist&card_id=<?php echo $get_event[0]->event_card_id; ?>&event_id=<?php echo $get_event[0]->event_no; ?>" class="full-div-link"></a>
+          <a href="/user-dashboard/?dashboard=preview&card_id=<?php echo $get_event[0]->event_card_id; ?>&event_id=<?php echo $get_event[0]->event_no; ?>" class="full-div-link"></a>
           <div class="inner">
             <div class="title-box">
               <div class="title graph">
-                <h4><a href="/user-dashboard/?dashboard=guestlist&card_id=<?php echo $get_event[0]->event_card_id; ?>&event_id=<?php echo $get_event[0]->event_no; ?>" class="text-black">Guest List</a></h4>
+                <h4><a href="/user-dashboard/?dashboard=preview&card_id=<?php echo $get_event[0]->event_card_id; ?>&event_id=<?php echo $get_event[0]->event_no; ?>" class="text-black">Guest List</a></h4>
               </div>
             </div>
             <div class="graph-box">
