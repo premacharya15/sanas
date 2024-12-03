47c1093c63939e098beb7a6cd3825dd9c15e0e39
parthwps <parth@netdevs.net>
2024-12-04 02:12:22 +0530

    Update cover.php

--- template-parts/dashboard/cover.php
+++ template-parts/dashboard/cover.php
@@ -1,3 +1,8 @@
+    <style>
+        #preloder-overlay{
+            display: block;
+        }
+    </style>
     <div class="wl-left-slide-bar">
         <div class="d-flex inner-colum">
             <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
