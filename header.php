f388a23a840ce5ca9550936df2248c25828d9efd
parthwps <parth@netdevs.net>
2024-12-06 08:16:49 +0530

    Update header.php

--- header.php
+++ header.php
@@ -22,14 +22,15 @@
 
 	<?php if ($is_guest_preview):
 		$eventtitle = esc_html(get_post_meta($edit_id, 'event_name', true));
+		$current_url = "http" . (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' ? "s" : "") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 	?>
 		
 	<!-- Open Graph Meta Tags -->
 	<meta name="og:title" content="You are invited to <?php echo htmlspecialchars($eventtitle); ?>" />
     <meta name="og:description" content="Tap to RSVP " />
     <meta property="og:type" content="website" />
-    <meta property="og:url" content="<?php echo $eventUrl; ?>" />
-    <meta property="og:image" content="<?php echo $eventImage; ?>" />
+    <meta property="og:url" content="<?php echo $current_url; ?>" />
+    <meta property="og:image" content="https://sit132.sanasinvite.com/wp-content/uploads/2024/08/Sana__s_Hub.png.png" />
     <meta property="og:image:width" content="600" />
     <meta property="og:image:height" content="300" />
     <?php endif; ?>
