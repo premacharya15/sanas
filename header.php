f8ba2bbe1c7af52d886ac8d3b4e759071db0f0f7
parthwps <parth@netdevs.net>
2024-12-07 01:49:24 +0530

    Update header.php

--- header.php
+++ header.php
@@ -40,15 +40,20 @@ $get_event_date = $wpdb->get_results($wpdb->prepare(
 	"SELECT * FROM $sanas_card_event_table WHERE event_no = %d",
 	$event_id
 ));
-// $event_rsvp_id = $get_event_date[0]->event_rsvp_id;
-// $guestName = esc_html(get_post_meta($event_rsvp_id, 'guest_name', true));
+$event_rsvp_id=$get_event_date[0]->event_rsvp_id;
+
+$get_event_date = $wpdb->get_results($wpdb->prepare(
+	"SELECT * FROM $sanas_card_event_table WHERE event_no = %d",
+	$event_id
+));
+
 		$eventtitle = esc_html(get_post_meta($event_rsvp_id, 'event_name', true));
 		$current_url =$current_url = "http" . (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' ? "s" : "") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";		;
 	?>
 		
 	<!-- Open Graph Meta Tags -->
 	<meta name="og:title" content="You are Invited! Click here to RSVP <?php echo htmlspecialchars($eventtitle); ?>" />
-    <meta name="og:description" content="Hema and Ariv has invited you to join <?php echo htmlspecialchars($eventtitle); ?> on <?php echo $get_event_date[0]; ?>" />
+    <meta name="og:description" content="Host name has invited you to <?php echo htmlspecialchars($eventtitle); ?> on <?php echo $get_event_date; ?> " />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="<?php echo $current_url; ?>" />
     <meta property="og:image" content="https://sit132.sanasinvite.com/wp-content/uploads/2024/08/Sana__s_Hub.png.png" />
