c9b6d1356fa90b3edd9ca4ea3d134e9aa9343fcc
parthwps <parth@netdevs.net>
2024-12-09 09:26:43 +0530

    update

--- assets/js/backend-canvas-editor.js
+++ assets/js/backend-canvas-editor.js
@@ -1,7 +1,7 @@
 var canvas = new fabric.Canvas('canvas');
 fabric.Object.prototype.transparentCorners = false;
 fabric.Object.prototype.cornerColor = '#511F1B';
-fabric.Object.prototype.cornerStyle = 'square';
+fabric.Object.prototype.cornerStyle = 'circle';
 fabric.Object.prototype.cornerSize = 14;
 fabric.Object.prototype.borderScaleFactor = 3;
 fabric.Object.prototype.borderColor = 'yellow';
