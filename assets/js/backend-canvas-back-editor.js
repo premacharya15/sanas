d886f571d388dbf6f87fd5277bab07c98d941901
parthwps <parth@netdevs.net>
2024-12-09 09:13:00 +0530

    Update backend-canvas-back-editor.js

--- assets/js/backend-canvas-back-editor.js
+++ assets/js/backend-canvas-back-editor.js
@@ -1,10 +1,10 @@
 var backcanvas = new fabric.Canvas('backcanvas');
 fabric.Object.prototype.transparentCorners = false;
-fabric.Object.prototype.cornerColor = 'white';
+fabric.Object.prototype.cornerColor = 'brown';
 fabric.Object.prototype.cornerStyle = 'circle';
 fabric.Object.prototype.cornerSize = 14;
 fabric.Object.prototype.borderScaleFactor = 3;
-fabric.Object.prototype.borderColor = 'purple';
+fabric.Object.prototype.borderColor = 'white';
 async function loadGoogleFonts() {
     const apiKey = 'AIzaSyB0FLGd0rxWqu7vC0nRvxjehyNge4SSFbE'; // Replace with your Google Fonts API key
     const apiUrl = `https://www.googleapis.com/webfonts/v1/webfonts?key=${apiKey}`;
