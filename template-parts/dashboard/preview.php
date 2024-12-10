92dfff66ac7322122822b27c96416c903a000d83
parthwps <parth@netdevs.net>
2024-12-10 09:37:06 +0530

    Update preview.php

--- template-parts/dashboard/preview.php
+++ template-parts/dashboard/preview.php
@@ -281,7 +281,7 @@ function is_youtube_url($url) {
 
                                         ?>
                                         
-                                        <div class="map-input-rsvp mt-2 mb-2 edit-text rsvp-msg" id="search_address" rows="2" cols="50" placeholder="Venue Address">61020 Petriano, Province of Pesaro and Urbino, Italy</textarea>
+                                        <div class="map-input-rsvp mt-2 mb-2 edit-text rsvp-msg" id="search_address" rows="2" cols="50" placeholder="Venue Address">61020 Petriano, Province of Pesaro and Urbino, Italy</div>
                                         <h4 class="mb-2" style="font-size:20px;color: #5c310d;">Hosted By</h4>
                                         <?php 
                                             if(!empty($guestName)) { echo '<div class="preview-host-name mb-2" style="'.$guest_name_css.'">'.esc_html($guestName).'</div>'; }
