b0cdca633e6ef09d00d104b543cbb5514ee1bbe2
Prem Acharya <166224472+premacharya15@users.noreply.github.com>
2024-12-11 17:26:14 +0530

    Update forntend-canvas-editor.js

--- assets/js/forntend-canvas-editor.js
+++ assets/js/forntend-canvas-editor.js
@@ -675,7 +675,6 @@ function loadCanvasFromJSON(json) {
                             if (familyName === obj.fontFamily) {
                                 obj.set({ fontFamily: familyName });
                                 canvas.renderAll();
-                                console.log('fontFamily', familyName);
                             }
                         }
                     });
