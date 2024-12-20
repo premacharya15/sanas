231748ebcb6289f106266c864d7a1b6fe330af35
Prem Acharya <166224472+premacharya15@users.noreply.github.com>
2024-12-20 17:14:51 +0530

    Update forntend-canvas-editor.js

--- assets/js/forntend-canvas-editor.js
+++ assets/js/forntend-canvas-editor.js
@@ -94,10 +94,10 @@ async function loadGoogleFonts() {
             option.text = font.family;
             option.value = font.family.replace(/ /g, '+'); // Replace spaces with '+'
             select.appendChild(option);
+            console.log('frontend-canvas-editor.js - font family 3:', font.family);
         });
         canvas.renderAll();
         console.log("test");
-        console.log('frontend-canvas-editor.js - font family 3:', font.family);
     } catch (error) {
         console.error('Error fetching Google Fonts:', error);
     }
