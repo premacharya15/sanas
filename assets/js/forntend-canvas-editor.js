6ae2530680c897cfbd8a61ae5a0de636726dd25f
parthwps <parth@netdevs.net>
2024-12-21 09:09:12 +0530

    Update forntend-canvas-editor.js

--- assets/js/forntend-canvas-editor.js
+++ assets/js/forntend-canvas-editor.js
@@ -512,7 +512,7 @@ jQuery(document).ready(function ($) {
 
     function loadCanvasData() {
         // Replace with your actual canvas data
-        let canvasData = canvasss;
+        let canvasData = JSON.parse(canvasss);
 
         try {
             let data = parseCanvasData(canvasData); // Try parsing the canvas data
