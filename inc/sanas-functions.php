8ba404a37d8e40c829a4475a274a5960b26cbf08
Prem Acharya <premcredsoft@gmail.com>
2024-12-24 20:39:31 +0530

    Update sanas-functions.php

--- inc/sanas-functions.php
+++ inc/sanas-functions.php
@@ -974,7 +974,7 @@ function sanas_get_card_preview_popup() {
                           </div>
                           <div class="preview-content">
                               <div class="preview-container">
-                                  <div class="flipper">
+                                  <div class="">
                                       <div class="front">
                                           <div id="cover-preview" class="preview-tab-content active">
                                               <!-- Front image will be loaded here -->
