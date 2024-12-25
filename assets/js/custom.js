f61df242062eecdf5df1e9874f84faf4d24df69e
Prem Acharya <premcredsoft@gmail.com>
2024-12-25 17:06:15 +0530

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1051,7 +1051,7 @@ jQuery(document).ready(function($) {
         
         if (frontImage) {
             $('#cover-preview').html(`
-                <div class="preview-image" aspect-ratio: 1;">
+                <div class="preview-image" style="aspect-ratio: 1;">
                     <img src="${frontImage}" alt="Front design" class="img-fluid flipper animated" style="width: auto;">
                 </div>
             `);
@@ -1059,7 +1059,7 @@ jQuery(document).ready(function($) {
         
         if (backImage) {
             $('#detail-preview').html(`
-                <div class="preview-image" aspect-ratio: 1;">
+                <div class="preview-image" style="aspect-ratio: 1;">
                     <img src="${backImage}" alt="Back design" class="img-fluid flipper animated" style="width: auto;">
                 </div>
             `);
