3f314cab96543fa1f634bc2cf8393aaa9c6b5581
Prem Acharya <premcredsoft@gmail.com>
2024-12-31 13:08:50 +0530

    Update sanas-functions.php

--- inc/sanas-functions.php
+++ inc/sanas-functions.php
@@ -1022,7 +1022,7 @@ function sanas_get_all_categories_popup() {
     ?>
     <div class="all-categories-popup-modal modal fade" id="all-categories-popup" tabindex="-1" role="dialog" aria-labelledby="allCategoriesModalTitle" aria-hidden="true">
         <div class="modal-dialog-centered modal-dialog" role="document">
-            <div class="modal-content custom-modal-height" style="max-height: 70vh;">
+            <div class="modal-content custom-modal-height">
                 <div class="modal-header">
                     <h5 class="modal-title" id="allCategoriesModalTitle">All Categories</h5>
                     <button type="button" class="all-categories-popup-close close" data-bs-dismiss="modal" aria-label="Close">
