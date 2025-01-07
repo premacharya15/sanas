c94c3fb415c97bbc93b94e7041b3bfabc9be6cab
parthwps <parth@netdevs.net>
2025-01-07 22:03:44 +0530

    Update forntend-canvas-editor.js

--- assets/js/forntend-canvas-editor.js
+++ assets/js/forntend-canvas-editor.js
@@ -25,10 +25,10 @@ function updateControlSizes() {
     customControls.duplicateControl.cornerSize = controlSize;
     canvas.renderAll();
 }
-var deletex = window.innerWidth <= 768 ? 0.6 : 0.5;
-var deletey = window.innerWidth <= 768 ? -0.65 : -0.5;
-var duplicatex = window.innerWidth <= 768 ? 0.6 : 0.5;
-var duplicatey = window.innerWidth <= 768 ? 0.65 : 0.5;
+var deletex = window.innerWidth <= 768 ? 0.65 : 0.5;
+var deletey = window.innerWidth <= 768 ? -0.75 : -0.5;
+var duplicatex = window.innerWidth <= 768 ? 0.65 : 0.5;
+var duplicatey = window.innerWidth <= 768 ? 0.75 : 0.5;
 var controlSize = window.innerWidth <= 768 ? 56 : 24;
 var customControls = {
     deleteControl: new fabric.Control({
