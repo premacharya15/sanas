2eebdc23d4841ed4e7305a25ef4a4cbe52fdb30f
parthwps <parth@netdevs.net>
2025-01-09 23:35:18 +0530

    Update header-dashboard.php

--- header-dashboard.php
+++ header-dashboard.php
@@ -14,7 +14,7 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <!-- <meta name="viewport" content="width=device-width, initial-scale=1, interactive-widget=resizes-content"> -->
-    sanas_preloading_overlay();
+    <?php sanas_preloading_overlay();?>
 
     <link rel="profile" href="https://gmpg.org/xfn/11">
     <style>
