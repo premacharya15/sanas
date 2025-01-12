2a1c9ca14b530b37dced583258882345f2564b23
parthwps <parth@netdevs.net>
2025-01-12 20:15:00 +0530

    Update rsvp.php

--- template-parts/dashboard/rsvp.php
+++ template-parts/dashboard/rsvp.php
@@ -958,12 +958,22 @@ if (empty($rsvpimage)) {
                                         ?>
                                           <tr>
                                               <td class="edit-text text-start" contenteditable="true" data-placeholder="Evening"></td>
-                                              <td class="edit-text text-start" contenteditable="true" data-placeholder="7:00 PM"></td>
+                                              <td class="edit-text text-start" contenteditable="true" data-placeholder="7 PM"></td>
                                               <td>
                                                 <button class="deleteRowBtn"><i class="fa-regular fa-trash-can"></i></button>
                                              </td>
                                              </tr>
                                    <?php }
+                                    else{
+                                        ?>
+                                            <tr>
+                                              <td class="edit-text text-start" contenteditable="true" data-placeholder="Evening"></td>
+                                              <td class="edit-text text-start" contenteditable="true" data-placeholder="7 PM"></td>
+                                              <td>
+                                                <button class="deleteRowBtn"><i class="fa-regular fa-trash-can"></i></button>
+                                             </td>
+                                             </tr>
+                                        <?php
                                     }
                                     ?>
                                     </tbody>
